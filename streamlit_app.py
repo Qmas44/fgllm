a3a5607174dfe5b2b5fc2f1ad9a69407a7c8991f
Quentin Terry <Qterry44@gmail.com>
2023-05-14 01:39:40 -0400

    Add training text and begining of data file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,22 @@
-import streamlit as st
-import pandas as pd
+import os, sys
+from pathlib import Path
 
-print('hello')
-st.write('hello')
+import streamlit as st
+
+if "OPENAI_API_KEY" not in os.environ:
+  print("You must set an OPENAI_API_KEY using the Secrets tool", file=sys.stderr)
+  exit()
+
+trainingData = list(Path('training/').rglob("**/*.*"))
+print(len(trainingData))
+
+if len(trainingData) < 1:
+    print("No files in the training folder", file=sys.stderr)
+    exit()
+data = []
+for training in trainingData:
+    with open(training) as f:
+        print(f"Add {f.name} to dataset")
+        data.append(f.read())
+
+st.write('helloooo')
