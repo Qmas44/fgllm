9063cdbc52790e1564a1141292e2c2cc3ed99643
Quentin Terry <Qterry44@gmail.com>
2023-05-14 03:24:14 -0400

    Create cache for faiss index and pickle

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,11 @@ import os, sys
 from pathlib import Path
 
 import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+import faiss
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+import pickle
 
 if "OPENAI_API_KEY" not in os.environ:
   print("You must set an OPENAI_API_KEY using the Secrets tool", file=sys.stderr)
@@ -18,5 +23,19 @@ for training in trainingData:
     with open(training) as f:
         print(f"Add {f.name} to dataset")
         data.append(f.read())
+
+textSplitter = CharacterTextSplitter(chunk_size=2000, separator="\n")
+
+docs = []
+for sets in data:
+    docs.extend(textSplitter.split_text(sets))
+
+store = FAISS.from_texts(docs, OpenAIEmbeddings())
+faiss.write_index(store.index, "training.index")
+store.index = None
+
+with open("training.store", "wb") as f:
+    store = pickle.dump(store, f)
+
 
 st.write('helloooo')
